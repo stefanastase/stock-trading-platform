fa3f1dd1b9165395f5942144858b39ad3c58978c
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-04-25 20:01:15 +0300

    Add POSTGRES, AUTH and ADMINER services to stack

---
+++ postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS clients (
+    "ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "ClientID" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "ClientSecret" character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    "IsAdmin" boolean NOT NULL,
+    CONSTRAINT clients_pkey PRIMARY KEY ("ID"),
+    CONSTRAINT "ClientID" UNIQUE ("ClientID")
+);
