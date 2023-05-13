ad7f9769c0d6fd4a73b95092f85fa259163bf746
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-05-13 13:30:20 +0300

    Add Order Management service and configure DB

--- postgres/init.sql
+++ postgres/init.sql
@@ -13,3 +13,41 @@ CREATE TABLE IF NOT EXISTS blacklist (
 );
 
 CREATE DATABASE portfolios;
+
+CREATE DATABASE orders;
+
+\connect orders
+
+CREATE TABLE IF NOT EXISTS placed (
+    "ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 1000 CACHE 1 ),
+    "ClientID" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "Symbol" character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    "Type" character(1) COLLATE pg_catalog."default" NOT NULL,
+    "Quantity" integer  NOT NULL,
+    "Price" real NOT NULL,
+    "PlacedAt" timestamp NOT NULL,
+    CONSTRAINT placed_pkey PRIMARY KEY ("ID")
+);
+
+CREATE INDEX placed_symbol
+ON placed("Symbol");
+
+CREATE INDEX placed_client
+ON placed("ClientID");
+
+CREATE TABLE IF NOT EXISTS executed(
+    "ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 1000 CACHE 1 ),
+    "ClientID" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "Symbol" character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    "Type" character(1) COLLATE pg_catalog."default" NOT NULL,
+    "Quantity" integer  NOT NULL,
+    "Price" real NOT NULL,
+    "ExecutedAt" timestamp NOT NULL,
+    CONSTRAINT executed_pkey PRIMARY KEY ("ID")
+);
+
+CREATE INDEX executed_symbol
+ON executed("Symbol");
+
+CREATE INDEX executed_client
+ON executed("ClientID");
