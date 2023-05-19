068f2fed0e08787cf3ca8c343cbc29672baf5b78
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-05-19 16:59:00 +0300

    Create an user for each service in Postgres and use secrets for sensible information

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,18 @@
+CREATE ROLE auth_user WITH LOGIN PASSWORD 'authPassword';
+CREATE ROLE portfolios_user WITH LOGIN PASSWORD 'portfoliosPassword';
+CREATE ROLE orders_user WITH LOGIN PASSWORD 'ordersPassword';
+
+CREATE DATABASE auth;
+GRANT ALL PRIVILEGES ON DATABASE auth to auth_user;
+
+CREATE DATABASE portfolios;
+GRANT ALL PRIVILEGES ON DATABASE portfolios to auth_user;
+GRANT ALL PRIVILEGES ON DATABASE portfolios to portfolios_user;
+
+CREATE DATABASE orders;
+GRANT ALL PRIVILEGES ON DATABASE orders to orders_user;
+
+\connect auth
 CREATE TABLE IF NOT EXISTS clients (
     "ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     "ClientID" character varying(128) COLLATE pg_catalog."default" NOT NULL,
@@ -6,15 +21,16 @@ CREATE TABLE IF NOT EXISTS clients (
     CONSTRAINT clients_pkey PRIMARY KEY ("ID"),
     CONSTRAINT "ClientID" UNIQUE ("ClientID")
 );
-
 CREATE TABLE IF NOT EXISTS blacklist (
     "token" character varying(256) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "token" UNIQUE ("token") 
 );
 
-CREATE DATABASE portfolios;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO auth_user;
 
-CREATE DATABASE orders;
+\connect portfolios
+GRANT ALL PRIVILEGES ON SCHEMA public TO auth_user;
+GRANT ALL PRIVILEGES ON SCHEMA public TO portfolios_user;
 
 \connect orders
 
@@ -51,3 +67,5 @@ ON executed("Symbol");
 
 CREATE INDEX executed_client
 ON executed("ClientID");
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO orders_user;
